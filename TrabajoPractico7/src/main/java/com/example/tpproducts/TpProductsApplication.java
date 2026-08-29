35abaf4b64db5f6193924b5b590170ab538a2e48
Ernesto Ramón López <ernestorlopez@hotmail.com.ar>
2026-08-29 19:44:21 -0300

    Resolucion del TP 7

---
+++ TrabajoPractico7/src/main/java/com/example/tpproducts/TpProductsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpProductsApplication.class, args);
+    }
+
+}
